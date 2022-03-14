71cae5f7f4acd48accb189cb5e710e1563d86d70
unknown <haymeemotoh22@gmail.com>
2022-03-14 12:10:28 +0800

    Screen Done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f25f6c75063f9266"; }
+/******/ 		__webpack_require__.h = function() { return "dc9943dd8a32928b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,38 @@ const index = () => {
             <div className="container">
                 <main>
                     <div className="calculator">
-                        Calculator
+                        <div className="calculator__screen">
+                            <div className="equation">000</div>
+                            <div className="answer">000</div>
+                        </div>
+                        <div className="calculator__keypad">
+                            <div className="buttonGroup__1 buttonGroup">
+                                <button>Light</button>
+                                <button>Dark</button>
+                            </div>
+                            <div className="buttonGroup__2 buttonGroup">
+                                <button>History</button>
+                                <button>Light</button>
+                                <button>Light</button>
+                                <button>Light</button>
+                            </div>
+                            <div className="buttonGroup__3 buttonGroup">
+                                <button>Light</button>
+                                <button>Light</button>
+                            </div>
+                            <div className="buttonGroup__4 buttonGroup">
+                                <button>Light</button>
+                                <button>Light</button>
+                            </div>
+                            <div className="buttonGroup__5 buttonGroup">
+                                <button>Light</button>
+                                <button>Light</button>
+                            </div>
+                            <div className="buttonGroup__6 buttonGroup">
+                                <button>Light</button>
+                                <button>Light</button>
+                            </div>
+                        </div>
                     </div>
                 </main>
             </div>
